507ec7c51da0cdbceec822adc83f0f8d6296eba7
João Roberto Fernandes Magalhães <120526440+joaorbrto@users.noreply.github.com>
2025-02-21 10:10:43 -0300

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Joao Roberto Fernandes Magalhaes on 21/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
