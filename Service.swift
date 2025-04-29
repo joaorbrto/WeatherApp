45a10fadf45b3166937bf125ab20c425ef73a607
João Roberto Fernandes Magalhães <120526440+joaorbrto@users.noreply.github.com>
2025-04-28 23:00:11 -0300

    introduzi a api

---
+++ Service.swift
@@ -0,0 +1,8 @@
+//
+//  Service.swift
+//  WeatherApp
+//
+//  Created by Joao Roberto Fernandes Magalhaes on 28/04/25.
+//
+
+import Foundation
---
+++ WeatherApp/Service.swift
@@ -0,0 +1,15 @@
+//
+//  Service.swift
+//  WeatherApp
+//
+//  Created by Joao Roberto Fernandes Magalhaes on 28/04/25.
+//
+
+import Foundation
+
+class Service {
+    private let baseURL: String = "https://api.openweathermap.org/data/3.0/onecall"
+    private let apiKey: String = "efb6ee0c150ee6c018caf25e1014bc7c"
+    
+    
+}
